a4c9441e1cf07cc9abae68cfc02c9a2833fc5e0c
dimasaritonang <dimasaritonang@gmail.com>
2015-06-22 23:41:53 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,26 @@
+<center>
+<form name="logForm" method="post" action="?open=Login-Validasi">
+  <table class="table-list" width="500" border="0" cellpadding="2" cellspacing="1">
+    <tr>
+      <td width="106" rowspan="4" align="center" bgcolor="#CCCCCC"><img src="images/login-key.png" width="116" height="75" /></td>
+      <th colspan="2"><b>LOGIN SISTEM INFORMASI </b></td>    
+    </tr>
+    <tr>
+      <td width="117"><b>Username</b></td>
+      <td width="263"><b>: 
+        <input name="txtUser" type="text" size="30" maxlength="20" />
+      </b></td>
+    </tr>
+    <tr>
+      <td><b>Password</b></td>
+      <td><b>: 
+        <input name="txtPassword" type="password" size="30" maxlength="20" />
+      </b></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td><input type="submit" name="btnLogin" value=" Login " /></td>
+      </tr>
+  </table>
+</form>
+</center>
